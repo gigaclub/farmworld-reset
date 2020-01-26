c34bb3ee5891ba9749a5036b88d517f1c0f81487
KevTVKevin <xxkevtvxx@gmail.com>
2020-01-26 15:34:54 +0100

    Initial commit

---
+++ src/main/java/gigaclub/farmworldreset/Main.java
@@ -0,0 +1,19 @@
+package gigaclub.farmworldreset;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Objects;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        System.out.println("Test");
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
